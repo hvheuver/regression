78b63ec002f3020243e9931e063b654b329a1e0f
hvheuver <hvheuver@mint.mint>
2017-08-08 01:11:01 +0200

    Added dataframe high-low and open-close

--- regression.py
+++ regression.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import quandl
 
-df = quandl.get('WIKI/GOOGL')
-
+dataframe = quandl.get('WIKI/GOOGL')
 
 # Print dataset
 # print(df.head())
+
+dataframe = dataframe[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+
+# High-low
+dataframe['HL'] = (dataframe['Adj. High'] - dataframe['Adj. Low']) / dataframe['Adj. Low']
+# Change between opening and closing price
+dataframe['change'] = (dataframe['Adj. Close'] - dataframe['Adj. Open']) / dataframe['Adj. Open']
+
+dataframe = dataframe[['Adj. Close', 'HL', 'change', 'Adj. Volume']]
+
+print(dataframe.head())
